f0c75e9781f81822bc33730b798023cab5b4144c
kuk0000 <kuk4815@gmail.com>
2024-08-26 14:28:23 +0900

    practice3

--- app/src/main/java/com/example/paractice1/MainActivity.kt
+++ app/src/main/java/com/example/paractice1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.paractice1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val tv : TextView = findViewById(R.id.tv_hello)
+        val btn : Button = findViewById(R.id.button_call)
+        btn.setOnClickListener{
+            tv.text = "안녕"
+        }
+
+//        val text: TextView = findViewById(R.id.android_text) as TextView
+//        text.setOnClickListener {
+//            text.setText(getString(R.string.name))
+//        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
